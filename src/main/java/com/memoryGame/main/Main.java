849e60fec7ce99ba161a998df01693fb18028f75
nomaan-ahmad <nomaan.ahmad21@gmail.com>
2023-02-21 15:52:34 +0530

    Bug fixes

--- src/main/java/com/memoryGame/main/Main.java
+++ src/main/java/com/memoryGame/main/Main.java
@@ -10,13 +10,19 @@ public class Main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("How many players are going to play?");
-            int numOfPlayers;
-            try {
-                numOfPlayers = Integer.parseInt(br.readLine());
-            } catch (Exception e) {
-                System.out.println("OOPS! It looks like you have selected wrong input.\nExiting the game...");
-                return;
+            int numOfPlayers = 2;
+            boolean flag = false;
+            while (!flag) {
+                try {
+                    numOfPlayers = Integer.parseInt(br.readLine());
+                    if (numOfPlayers >= 2) flag = true;
+                    else System.out.println("Select at least 2 players");
+                } catch (Exception e) {
+                    System.out.println("OOPS! It looks like you have selected wrong input.\nExiting the game...");
+                    return;
+                }
             }
+
             System.out.println("Do you want to give player name? If not, system will assign unique name to the players");
             System.out.println("Type 'Yes' or 'NO'! Wrong input will select NO by default");
             Player p;
--- src/main/java/com/memoryGame/user/Player.java
+++ src/main/java/com/memoryGame/user/Player.java
@@ -5,24 +5,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Player {
-    private List<String> players;
+    private final List<String> players;
     private final int numOfPlayers;
     public Player(int numOfPlayers, boolean customName) {
         this.numOfPlayers = numOfPlayers;
+        players = new ArrayList<>();
         if (customName) playerName();
         else createRandomName();
     }
 
     private void playerName() {
-        System.out.println("Kindly type " + numOfPlayers + " player's name...");
+        System.out.println("Kindly type " + numOfPlayers + " player name...");
         Scanner in = new Scanner(System.in);
         for (int i = 0; i < numOfPlayers; i++) {
-            players.add(in.nextLine());
+            String name = in.nextLine();
+            players.add(name);
         }
     }
     private void createRandomName(){
         RandomNameGenerator r = new RandomNameGenerator();
-        players = new ArrayList<>();
         for (int i = 0; i < numOfPlayers; i++) {
             players.add(r.randomName());
         }
